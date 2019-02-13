49cae8660690ecea04422dc6fd2e08afa16d7e92
tinaun <tinagma@gmail.com>
2019-02-12 22:58:58 -0500

    intervals!

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,14 @@
 #![feature(futures_api, async_await, await_macro)]
 
 
-use std::time::Duration;
+use std::time::{Duration, Instant};
 use std::sync::{Arc, Mutex};
 
 use std::pin::Pin;
 
 use futures::prelude::*;
 use futures::future::FusedFuture;
+use futures::stream::FusedStream;
 use futures::task::{Poll, LocalWaker, Waker};
 
 #[macro_export]
@@ -22,7 +23,7 @@ macro_rules! dbg_println {
 
 #[path = "sys/windows.rs"]
 mod imp;
-use imp::Timer;
+use imp::NativeTimer;
 
 #[derive(Debug)]
 pub (crate) struct TimerState {
@@ -43,42 +44,60 @@ impl TimerState {
     }
 }
 
+#[derive(Debug)]
+struct Timer {
+    handle: NativeTimer,
+    state: Arc<Mutex<TimerState>>,
+}
+
+impl Timer {
+    pub fn new() -> Self {
+        let state = Arc::new(Mutex::new(TimerState::new()));
+
+        unsafe {
+            let ptr = Arc::into_raw(state);
+            let handle = NativeTimer::new(ptr as *mut _);
+            let state = Arc::from_raw(ptr);
+
+
+            Timer {
+                handle,
+                state,
+            }
+        }
+    }
+
+    fn register_waker(&mut self, lw: &LocalWaker) {
+        let mut state = self.state.lock().unwrap();
+        state.register_waker(lw);
+    }
+
+    fn is_active(&self) -> bool {
+        self.handle.is_active()
+    }
+
+    fn is_done(&self) -> bool {
+        self.state.lock().unwrap().done
+    }
+}
+
 
 #[derive(Debug)]
 #[must_use = "futures do nothing unless polled"]
 pub struct Delay {
-    handle: Timer,
-    inner: Arc<Mutex<TimerState>>,
+    inner: Timer,
     delay: Duration,
     done: bool,
 }
 
 impl Delay {
     pub fn new(delay: Duration) -> Self {
-        let state = Arc::new(Mutex::new(TimerState::new()));
+        let inner = Timer::new();
 
-        unsafe {
-            let ptr = Arc::into_raw(state);
-            let handle = Timer::new(ptr as *mut _);
-            let inner = Arc::from_raw(ptr);
-
-
-            Delay {
-                handle,
-                inner,
-                delay,
-                done: false,
-            }
-        }
-    }
-
-    fn register_waker(&mut self, lw: &LocalWaker) {
-        let mut state = self.inner.lock().unwrap();
-        state.register_waker(lw);
-        
-        
-        if !self.handle.is_active() {
-            self.handle.init_delay(self.delay);
+        Delay {
+            inner,
+            delay,
+            done: false,
         }
     }
 }
@@ -87,8 +106,13 @@ impl Future for Delay {
     type Output = ();
 
     fn poll(mut self: Pin<&mut Self>, lw: &LocalWaker) -> Poll<Self::Output> {
-        self.register_waker(lw);
-        if self.inner.lock().unwrap().done {
+        if !self.inner.is_active() {
+            let delay = self.delay;
+            self.inner.handle.init_delay(delay);
+        }
+
+        self.inner.register_waker(lw);
+        if self.inner.is_done() {
             self.done = true;
             Poll::Ready(())
         } else {
@@ -106,8 +130,54 @@ impl FusedFuture for Delay {
 impl Unpin for Delay {}
 
 
+pub struct Interval {
+    inner: Timer,
+    interval: Duration,
+}
+
+impl Interval {
+    pub fn new(interval: Duration) -> Self {
+        let inner = Timer::new();
+
+        Interval {
+            inner,
+            interval,
+        }
+    }
+}
+
+impl Stream for Interval {
+    type Item = Instant;
+
+    fn poll_next(mut self: Pin<&mut Self>, lw: &LocalWaker) -> Poll<Option<Self::Item>> {
+        if !self.inner.is_active() {
+            let interval = self.interval;
+            self.inner.handle.init_interval(interval);
+        }
+
+        self.inner.register_waker(lw);
+        let mut state = self.inner.state.lock().unwrap();
+        if state.done {
+            state.done = false;
+            Poll::Ready(Some(Instant::now()))
+        } else {
+            Poll::Pending
+        }
+    }
+}
+
+impl FusedStream for Interval {
+    fn is_terminated(&self) -> bool {
+        false
+    }
+}
+
+impl Unpin for Interval {}
+
+
 #[cfg(test)]
 mod tests {
+    use futures::prelude::*;
     use futures::executor::block_on;
     use std::time::{Duration, Instant};
     use super::*;
@@ -145,6 +215,30 @@ mod tests {
 
         let res = block_on(work);
         assert_eq!(res, "short finished first");
+    }
+
+    #[test]
+    fn intervals() {
+        use futures::select;
+
+        let mut timeout = Delay::new(Duration::from_secs(1));
+        let mut stream = Interval::new(Duration::from_millis(95));
+
+        let work = async {
+            let mut total = 0;
+            loop {
+                select! {
+                    _ = stream.next() => total += 1,
+                    _ = timeout => break,
+                }
+            }
+            
+
+            total
+        };
+
+        let res = block_on(work);
+        assert_eq!(res, 10);
     }
 }
 
--- src/sys/windows.rs
+++ src/sys/windows.rs
@@ -22,21 +22,21 @@ unsafe extern "system" fn timer_callback(_instance: PTP_CALLBACK_INSTANCE, conte
     let context = context as *mut Mutex<TimerState>;
     let mut state = (*context).lock().unwrap();
     state.done = true;
-    state.wake.as_ref().unwrap().wake();
+    state.wake.as_ref().map(|w| w.wake());
 
 }
 
 #[derive(Debug)]
-pub struct Timer {
+pub struct NativeTimer {
     inner: PTP_TIMER,
     active: bool,
 }
 
-impl Timer {
+impl NativeTimer {
     pub(crate) unsafe fn new( state: *mut Mutex<TimerState> ) -> Self {
         let timer = CreateThreadpoolTimer(Some(timer_callback), state as *mut _ , ptr::null_mut());
 
-        Timer {
+        NativeTimer {
             inner: timer,
             active: false,
         }
@@ -47,24 +47,37 @@ impl Timer {
     }
 
     pub fn init_delay(&mut self, delay: Duration) {
-        self.active = true;
-        dbg_println!("timer started!");
         let mut ticks = (delay.subsec_nanos() / 100) as i64;
         ticks += (delay.as_secs() * 10_000_000) as i64;
         let ticks = -ticks;
+
+        self.init(ticks, 0);
+    }
+
+    pub fn init_interval(&mut self, interval: Duration) {
+        let mut ticks = (interval.subsec_nanos() / 100) as i64;
+        ticks += (interval.as_secs() * 10_000_000) as i64;
+        let millis = (ticks / 10_000) as u32;
+        let ticks = -ticks;
+        
+        self.init(ticks, millis);
+    } 
+
+    fn init(&mut self, start: i64, repeat: u32) {
+        self.active = true;
+        dbg_println!("timer started!");
 
         unsafe {
             // i need to find a better way to do this. :/
             // probably byteorder? windows apis are super weird - where else would a i64 
             // have to be represented as two u32s
-            let mut time: FILETIME = std::mem::transmute(ticks);
-            SetThreadpoolTimerEx(self.inner, &mut time, 0, 0);
+            let mut time: FILETIME = std::mem::transmute(start);
+            SetThreadpoolTimerEx(self.inner, &mut time, repeat, 0);
         }
-        
     } 
 }
 
-impl Drop for Timer {
+impl Drop for NativeTimer {
     fn drop(&mut self) {
         unsafe {
             SetThreadpoolTimerEx(self.inner, ptr::null_mut(), 0, 0);
